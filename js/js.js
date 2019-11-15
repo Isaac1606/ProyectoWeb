6671491755fd919ea2bf77b84ed578c3603c766f
Isaac1606 <isaac-dx@live.com.mx>
2019-11-15 13:00:59 -0600

    commit

--- js/js.js
+++ js/js.js
@@ -7,6 +7,16 @@ $('.custom-file-input').on('change', function() {
         formdata.append(fileName, file);
     });
     console.log(formdata);
+    let preview = document.querySelector('#imagen1');
+    let file    = document.querySelector('input[type=file]').files[0];
+    let reader = new FileReader();
+    reader.addEventListener("load", function () {
+        preview.src = reader.result;
+    }, false);
+
+    if (file) {
+        reader.readAsDataURL(file);
+    }
     $.ajax({
         url: "/my.php", //direccion del archivo php al que se lo mandaras
         data : formdata,
@@ -24,5 +34,17 @@ $('.custom-file-input').on('change', function() {
     });
     return false;
 });
+function previewFile() {
+    var preview = document.querySelector('img');
+    var file    = document.querySelector('input[type=file]').files[0];
+    var reader  = new FileReader();
 
+    reader.addEventListener("load", function () {
+        preview.src = reader.result;
+    }, false);
+
+    if (file) {
+        reader.readAsDataURL(file);
+    }
+}
 // $file = $_FILES['image_pe']; codigo php
