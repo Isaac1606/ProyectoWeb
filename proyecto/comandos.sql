24dbf2ac40a6d509701fa4311f50771b2254efc7
Braulio Dominguez <44845854+brolisdom@users.noreply.github.com>
2019-11-16 23:18:36 -0600

    Add files via upload

---
+++ proyecto/comandos.sql
@@ -0,0 +1,17 @@
+drop table usuario;
+
+create table usuario(
+    id_usuario varchar(20) not null,
+    correo varchar(50) not null,
+    nombre varchar(20) not null,
+    apellido1 varchar(20),
+    apellido2 varchar(20),
+    clave varchar(20) not null,
+    genero varchar(10) not null,
+    fecha_nacimiento date not null,
+    fecha_registro date not null,
+    tipo varchar(10) not null,
+    constraint llave_primaria primary key (id_usuario,correo)
+);
+
+insert into usuario values("juanito","juanito@mail.com","juan","lopez","lopez","12345","hombre","1999-04-16",curdate(),"admin");
